a27f9be6147aeec2f8122951e23f514301c8dcb7
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-04-06 12:53:27 +0000

    initial commit

---
+++ src/main/java/com/example/springboot_app_testunitaire/SpringbootAppTestunitaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_app_testunitaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppTestunitaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppTestunitaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_app_testunitaire/SpringbootAppTestunitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_app_testunitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppTestunitaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
